482b61a0c74e379dff00760135e9f2bccaec0288
Soumalya Bhattacharyya <122465630+soubhatta@users.noreply.github.com>
2023-08-16 04:20:49 +0530

    Add files via upload

---
+++ Task 2_Music Player/script.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Phir Le Aya Dil',
+        path: 'musics/Song 1.mp3',
+        artist: 'Arijit Singh',
+        cover: 'images/ARIJIT.png'
+    },
+    {
+        name: 'Aaj Jane Ki Zid',
+        path: 'musics/Song 2.mp3',
+        artist:     'Papon',
+        cover: 'images/PAPON.png'
+    },
+    {
+        name: 'Aaoge Jab Tum',
+        path: 'musics/Song 3.mp3',
+        artist:'Rashid Khan',
+        cover: 'images/RASHID.png'
+    },
+    {
+        name: 'Tum Hi Dekho Na',
+        path: 'musics/Song 4.mp3',
+        artist:    'Sonu Nigam',
+        cover: 'images/SONU.png'
+    },
+    {
+        name: 'Laut Aa Mere Des',
+        path: 'musics/Song 5.mp3',
+        artist:'Shreya Ghoshal',
+        cover: 'images/SHREYA.png'
+    },
+]
